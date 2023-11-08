a6345b8840a18d6931379d90b763a37d3652a867
anas <anasazzouz8@gmail.com>
2023-11-08 13:05:11 +0100

    update

--- general_reports/models.py
+++ general_reports/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
 class Profitability(models.Model):
+    id = models.AutoField(primary_key=True)  # Add this line to define a primary key
     date = models.DateField(_("date"), null=True, blank=True)
     type = models.CharField(_("type"), max_length=20, default="sales")
     value = models.DecimalField(_("Value"), max_digits=19, decimal_places=2, default=0)
